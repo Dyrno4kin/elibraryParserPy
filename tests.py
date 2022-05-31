81042e0c3b12d5c4935896fac597687dd6070225
A Dyrkin <adresat277@gmail.com>
2022-05-31 19:15:45 +0400

    add second test

--- tests.py
+++ tests.py
@@ -5,3 +5,7 @@ def test_answer():
     with open('resourse/file.csv', newline='') as csvfile:
         dfSource = pd.DataFrame(csv.reader(csvfile, delimiter=',', quotechar='|'))
         assert  len(dfSource) == 2026
+
+def test_get_api_endpoint(self):
+    r = self.app.get('/api/statistics')
+    self.assertEqual(r.status_code, 200)
